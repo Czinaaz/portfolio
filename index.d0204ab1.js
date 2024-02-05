291cbbe06197461ca2219a2086b56a8e243a43a0
Czinaaz <130914798+Czinaaz@users.noreply.github.com>
2024-02-05 08:48:34 +0000

    Deploying to gh-pages from @ Czinaaz/portfolio@5079db258925ef581ae3c03a397184fe3da6989c 🚀

---
+++ index.d0204ab1.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var c=document.querySelectorAll(".image-list_item");c.forEach((function(e){e.addEventListener("click",(function(){this.classList.contains("clicked")?this.classList.remove("clicked"):(c.forEach((function(c){c.classList.remove("clicked")})),this.classList.add("clicked"))}))}))}));
+//# sourceMappingURL=index.d0204ab1.js.map
