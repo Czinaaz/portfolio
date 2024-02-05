5079db258925ef581ae3c03a397184fe3da6989c
Ivan Rudenko <rudenko.ivan81@gmail.com>
2024-02-05 09:47:55 +0100

    save

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,19 +1,19 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var imageItems = document.querySelectorAll('.image-list_item');
+document.addEventListener('DOMContentLoaded', function() {
+    var imageItems = document.querySelectorAll('.image-list_item');
   
-//     imageItems.forEach(function(item) {
-//       item.addEventListener('click', function() {
-//         if (this.classList.contains('clicked')) {
-//           this.classList.remove('clicked');
-//         } else {
-//           // Usuń klasę 'clicked' ze wszystkich elementów
-//           imageItems.forEach(function(el) {
-//             el.classList.remove('clicked');
-//           });
+    imageItems.forEach(function(item) {
+      item.addEventListener('click', function() {
+        if (this.classList.contains('clicked')) {
+          this.classList.remove('clicked');
+        } else {
+          // Usuń klasę 'clicked' ze wszystkich elementów
+          imageItems.forEach(function(el) {
+            el.classList.remove('clicked');
+          });
          
-//           this.classList.add('clicked');
-//         }
-//       });
-//     });
-//   });
+          this.classList.add('clicked');
+        }
+      });
+    });
+  });
   
