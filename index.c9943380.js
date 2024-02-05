36c4577ebcfd68d72742f500acb4c34b35a92aa5
Czinaaz <130914798+Czinaaz@users.noreply.github.com>
2024-02-05 20:29:46 +0000

    Deploying to gh-pages from @ Czinaaz/portfolio@98cd8fad401684b7f495dec529c67a4985368c98 🚀

---
+++ index.c9943380.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var o=document.getElementById("scrollUpBtn");window.addEventListener("scroll",(function(){window.scrollY>200?o.classList.add("show"):o.classList.remove("show")})),o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}))}));
+//# sourceMappingURL=index.c9943380.js.map
